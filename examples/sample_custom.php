6ba2943e88a315d606affec01a779e1a41cb9b60
Tim Nance <tmnance@gmail.com>
2016-02-26 15:56:18 -0500

    Custom sample enhancements

--- examples/sample_custom.php
+++ examples/sample_custom.php
@@ -5,21 +5,44 @@ use CrosswordPuzzle\Builder as PuzzleBuilder;
 
 
 $words_raw = (array_key_exists('words', $_POST) ? $_POST['words'] : '');
+?>
+
+<table cellspacing="0" cellpadding="10" border="0">
+    <tr>
+        <td valign="top">
+            <h3>Words to process</h3>
+            <form action="sample_custom.php" method="post">
+                <p>
+                    <textarea name="words" style="width: 250px; height: 250px;"><?php echo $words_raw;?></textarea>
+                </p>
+                <input type="submit" />
+            </form>
+        </td>
+        <td valign="top">
+            <h3>Generated Crossword</h3>
+
+<?php
+echo '<pre>';
 if (!empty($words_raw)) {
     // process form words
-    echo '<pre>';
     $words_raw = $_POST['words'];
     // remove garbage chars
     $words_raw = str_replace("\r", '', $words_raw);
     $words_list = explode("\n", $words_raw);
+
+    // cleanup input
+    $words_list = array_map('trim', $words_list);
+    $words_list = array_filter($words_list);
+
+    // words themselves will be the array keys
     $words_list = array_flip($words_list);
+
     // assign filler clues
     foreach ($words_list as $key => $value) {
         $words_list[$key] = "{$key} definition";
     }
 
     try {
-
         $puzzle = (new PuzzleBuilder())->setSource($words_list);
         $best_result_grid = $puzzle->getBestScoreGrid();
 
@@ -27,22 +50,17 @@ if (!empty($words_raw)) {
 
         $best_result_grid->debug();
     } catch (Exception $e) {
-        echo "{$e->getMessage()}\n";
+        echo "{$e->getMessage()}\n\n";
     }
 
-    echo "done\n\n";
-    echo '</pre>';
+    echo "done";
+} else {
+    echo "no input";
 }
-
-
-echo <<<END
-<h3>Words to process</h3>
-<form action="sample_custom.php" method="post">
-    <p>
-        <textarea name="words" style="width: 250px; height: 250px;">{$words_raw}</textarea>
-    </p>
-    <input type="submit" />
-</form>
-END;
+echo '</pre>';
+?>
+        </td>
+    </tr>
+</table>
 
 
